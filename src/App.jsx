d447a352cfbcc68fcaf7328434ebb74e004e86d1
Sadam-jay <sadamjay4@gmail.com>
2025-01-24 22:19:09 +0530

    add filter and sort functionality

--- src/App.jsx
+++ src/App.jsx
@@ -18,18 +18,27 @@ function App() {
       quantity: 10,
     },
     {
+      id: 4,
+      name: "Light",
+      category: "Electronics",
+      quantity: 20,
+    },
+    {
       id: 3,
       name: "Mouse",
       category: "Electronics",
       quantity: 15,
     },
     {
-      id: 4,
-      name: "Light",
-      category: "Electronics",
-      quantity: 20,
+      id: 5,
+      name: "Desk",
+      category: "Wooden",
+      quantity: 15,
     },
   ]);
+
+  const [sortAsc, setSortAsc] = useState(true);
+  const [filter, setFilter] = useState("");
 
   const addItem = (newItem) => {
     setItems([...items, { id: Date.now(), ...newItem }]);
@@ -43,12 +52,36 @@ function App() {
     setItems(items.filter((item) => item.id !== id));
   };
 
+  const filteredItems = filter
+    ? items.filter((item) => item.category === filter)
+    : items;
+
+  const sortedItems = filteredItems.sort((a, b) =>
+    sortAsc ? a.quantity - b.quantity : b.quantity - a.quantity
+  );
+
   return (
     <div className="container">
       <h1 className="heading">Inventory Management</h1>
       <InventoryForm addItem={addItem} />
+      <button className="sort-btn" onClick={() => setSortAsc(!sortAsc)}>
+        Sort by Quantity ({sortAsc ? "Ascending" : "Descending"})
+      </button>
+      <select
+        className="filter-list"
+        onChange={(e) => setFilter(e.target.value)}
+      >
+        <option value="">All Category</option>
+        {[...new Set(items.map((item) => item.category))].map(
+          (category, index) => (
+            <option key={index} value={category}>
+              {category}
+            </option>
+          )
+        )}
+      </select>
       <InventoryTable
-        items={items}
+        items={sortedItems}
         updateItem={updateItem}
         deleteItem={deleteItem}
       />
